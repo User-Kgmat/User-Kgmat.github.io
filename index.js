89d7f4907efd508462bb04e13d1fedcf90012713
Kevin M <89092431+User-Kgmat@users.noreply.github.com>
2022-07-01 11:55:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//as builds are completed, replace gifs with full background images and videos of builds.
+
+function highlight(identification)
+{
+    idd = document.getElementById(identification);
+    idd.style.color = "darkgrey";
+    
+}
+function mouseleave(identification)
+{
+    ident = document.getElementById(identification);
+    ident.style.color = "black";
+}
+function onclick()
+{
+
+}
