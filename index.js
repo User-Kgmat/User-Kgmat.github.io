2296c90a6bd628f47144dfdadef92cd03925c8af
Kevin M <89092431+User-Kgmat@users.noreply.github.com>
2022-07-03 10:31:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ function highlight(identification)
 {
     idd = document.getElementById(identification);
     idd.style.color = "darkgrey";
+    if (idd.id == 'prebutton')
+    {
+        idd.style.color = "black";
+        idd.style.backgroundColor = "white";
+    }
+    else if (idd.id == 'budgetbutton')
+    {
+        idd.style.color = "black";
+        idd.style.backgroundColor = "white";
+    }
     
 }
 function mouseleave(identification)
 {
     ident = document.getElementById(identification);
     ident.style.color = "black";
+    if (ident.id == "prebutton")
+    {
+        ident.style.color = "white";
+        ident.style.backgroundColor = "black";
+    }
+    else  if (ident.id == "budgetbutton")
+    {
+        ident.style.color = "white";
+        ident.style.backgroundColor = "black";
+    }
 }
 function onclick()
 {
